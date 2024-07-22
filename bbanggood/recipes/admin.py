bb84e7de93397ba5dd311916c56ef2a177077cb8
SangHee-Hong <hongdabagi@gmail.com>
2024-07-23 02:38:24 +0900

    recipe에 필요한 모델(재료, 태그, 레시피, 과정) 초본

--- bbanggood/recipes/admin.py
+++ bbanggood/recipes/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class StepInline(admin.TabularInline):
+    model= Step
+    extra = 1
+# admin.py
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity')
+    search_fields = ('name',)
+
+@admin.register(RecipeTag)
+class RecipeTagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'cook_time', 'equipment')
+    inlines = [StepInline]
+    search_fields = ('title', 'author__username', 'ingredients__name', 'tag__name')
+    filter_horizontal = ('tag', 'ingredients')
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'order', 'instruction')
+    search_fields = ('recipe__title', 'instruction')
+    list_filter = ('recipe',)
--- bbanggood/recipes/models.py
+++ bbanggood/recipes/models.py
@@ -1,4 +1,58 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+
 # Create your models here.
+User = get_user_model()
+
+class Ingredient(models.Model):
+    name = models.CharField('재료명',max_length=100)
+    quantity = models.CharField('수량',max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity})"
+
+class RecipeTag(models.Model):
+    name = models.CharField(max_length=32, verbose_name='태그명')
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}"
+
+
+class Recipe(models.Model):
+
+    EQUIPMENT = [
+        ('microwave', '전자레인지'),
+        ('oven', '오븐'),
+        ('air_fryer', '에어프라이어'),
+    ]
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='작성자', null = True, blank=True)
+    title =  models.CharField('레시피명', max_length=100)
+    represent_image = models.ImageField('대표이미지')
+    tag = models.ManyToManyField(RecipeTag, blank=True)
+    cook_time = models.CharField('조리시간', max_length= 50)
+    equipment = models.CharField(max_length=50, choices = EQUIPMENT)
+    ingredients = models.ManyToManyField(Ingredient)
+
+
+class Step(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='steps', on_delete=models.CASCADE)
+    order = models.PositiveIntegerField(blank=True, null=True)
+    image = models.ImageField('이미지', blank=True, null=True)
+    instruction = models.TextField('내용')
+
+    class Meta:
+        ordering = ['order']
+
+    def save(self, *args, **kwargs):
+        if self.order is None:
+            last_step = Step.objects.filter(recipe=self.recipe).order_by('-order').first()
+            if last_step:
+                self.order = last_step.order + 1
+            else:
+                self.order = 1
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.order}: {self.instruction[:50]}"
 
